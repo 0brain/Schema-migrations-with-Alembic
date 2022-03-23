51c293d83e5778fa2ab21ab2b81015e39cccf66c
Nazar Khort <nazar.khort@gmail.com>
2022-03-23 12:23:37 +0200

    Add upgrade and downgarde functions

--- alembic/versions/028ac3023740_baseline.py
+++ alembic/versions/028ac3023740_baseline.py
@@ -16,9 +16,15 @@ from alembic import op
 import sqlalchemy as sa
 
 
-def upgrade():
-    pass
+def upgrade():  
+    op.create_table(
+        'bug',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('bug_tracker_url', sa.String(), nullable=False),
+        sa.Column('root_cause', sa.String()),
+        sa.Column('who', sa.String()),
+        sa.Column('when', sa.DateTime(), default=sa.func.now()))
 
 
-def downgrade():
-    pass
+def downgrade():  
+    op.drop_table('bug')
