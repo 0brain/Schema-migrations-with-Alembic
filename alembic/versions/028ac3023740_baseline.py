60fc5c56c3684e44e93d4e6e379ce124131a0caf
Nazar Khort <nazar.khort@gmail.com>
2022-03-23 12:21:20 +0200

    Create first revision

---
+++ alembic/versions/028ac3023740_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: 028ac3023740
+Revises: 
+Create Date: 2022-03-23 12:17:31.990607
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '028ac3023740'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
